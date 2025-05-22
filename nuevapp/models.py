d1dc5a881329d80233adb517adeb4d6b97b1cc4c
Marknejo Borja <marknejo246@gmail.com>
2025-05-22 14:29:15 -0500

    Creating and start models

--- nuevapp/models.py
+++ nuevapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
+class Color(models.Model):
+    name = models.CharField(max_length=100)
+    hex_value = models.CharField(max_length=7)
+
 # Create your models here.
